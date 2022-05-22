e1e0368cb9d9982164cec1b1cebab3e573115f70
Robin Choffardet <robin.choffardet@gmail.com>
2022-05-22 23:21:08 +0200

    passage en mode POO

--- src/Program.cs
+++ src/Program.cs
@@ -24,86 +24,12 @@ namespace App
 
             for(var i = 0; i < iterations; i++)
             {
-                UpdateQuality();
-            }
-        }
-
-        public static void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                foreach(var item in Items)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    item.UpdateQuality();
                 }
             }
         }
-
     }
 
     public class Item
@@ -113,6 +39,79 @@ namespace App
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
+
+        public void UpdateQuality()
+        {
+            if (this.Name != "Aged Brie" && this.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (this.Quality > 0)
+                {
+                    if (this.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        this.Quality = this.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+
+                    if (this.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.SellIn < 11)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+
+                        if (this.SellIn < 6)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (this.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                this.SellIn = this.SellIn - 1;
+            }
+
+            if (this.SellIn < 0)
+            {
+                if (this.Name != "Aged Brie")
+                {
+                    if (this.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Quality > 0)
+                        {
+                            if (this.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                this.Quality = this.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.Quality = this.Quality - this.Quality;
+                    }
+                }
+                else
+                {
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+                }
+            }
+        }
     }
 
 }
